295e6b59fab6726108be470692f7f4cc0dcae752
mufeng520 <78904973+mufeng520@users.noreply.github.com>
2024-03-24 13:28:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from lib.spider import beike_spider
+from lib.spider import beike_spider,lianjia_spider
+from lib.config.config import *
+work = {
+    LIANJIA_SPIDER:{
+        XIAOQU:lianjia_spider.XiaoQuBaseSpider,
+        LOUPAN:lianjia_spider.LouPanBaseSpider,
+        ERSHOUFANG:lianjia_spider.ErShouSpider,
+        ZUFANG:lianjia_spider.ZuFangBaseSpider
+    },
+    BEIKE_SPIDER: {
+        XIAOQU:beike_spider.XiaoQuBaseSpider,
+        LOUPAN:beike_spider.LouPanBaseSpider,
+        ERSHOUFANG:beike_spider.ErShouSpider,
+        ZUFANG:beike_spider.ZuFangBaseSpider
+    },
+}
 
 if __name__ == "__main__":
-    spider = beike_spider.LouPanBaseSpider()
+    spider = work.get(SPIDER_NAME).get(CLASS_NAME)()
     spider.start()
